96efa8c44eb90323cc5b062d653c6405eb05f945
Ashley Owens <ash4741@gmail.com>
2019-02-05 21:32:03 -0500

    Complete Async Redux Connection

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,24 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+import Axios from 'axios';
+
+export const FETCHING = 'FETCHING';
+export const SUCCESS = 'SUCCESS';
+export const ERROR = 'ERROR';
+
+
+export function getStarwarsCharacters() {
+  return dispatch => {
+    dispatch({ type: FETCHING });
+    
+    Axios.get(`https://swapi.co/api/people/`)
+    .then(response => {
+      dispatch({ type: SUCCESS, starWarsData: response.data.results });
+    })
+    .catch(error => {
+      dispatch({ type: ERROR, errorMessage: "Oops Something Went Wrong. Please Try Again!" });
+    })
+  };
+}
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import { getStarwarsCharacters } from "../actions";
 
 class CharacterListView extends React.Component {
   constructor() {
@@ -10,12 +11,15 @@ class CharacterListView extends React.Component {
   }
 
   componentDidMount() {
-    // call our action
+    console.log(this.props)
+    this.props.getStarwarsCharacters()
+    
   }
 
-  render() {
+  render() {console.log(this.props)
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return <h3>Fetching Your Starwars Data...</h3>
     }
     return (
       <div className="CharactersList_wrapper">
@@ -25,11 +29,17 @@ class CharacterListView extends React.Component {
   }
 }
 
+const mapStateToProps = state => {
+  return{
+    characters: state.charsReducer.characters,
+    fetching: state.charsReducer.fetching
+  }
+}
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  (mapStateToProps),
   {
-    /* action creators go here */
+    getStarwarsCharacters: getStarwarsCharacters
   }
 )(CharacterListView);
